8191bd4104e7affb1eaf2db14aaf214fbbd831b7
whobrien <whotemp@hotmail.com>
2022-03-10 13:03:21 -0500

    Excercise 2 completed All Test Passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let rString = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        rString += string;
+    }
+    return rString;
 };
 
 // Do not edit below this line
