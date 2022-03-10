5aed34c4c253650c25edecb4d2bd40baee1ee2b7
whobrien <whotemp@hotmail.com>
2022-03-10 13:55:10 -0500

    Excercise 3 completed All Test Passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let len = string.length - 1;
+    let rString = "";
+    for (i = 0; i <= len; i++) {
+        rString = rString + string[len - i];
+    }
+    return rString;
 };
 
 // Do not edit below this line
