366587937f827b33df861deec1b9dbff5b3faa74
whobrien <whotemp@hotmail.com>
2022-03-11 07:46:08 -0500

    Excercise 4 completed All Test Passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function() {
-
+    let el = Array.prototype.slice.call(arguments);
+    let array = el.shift();
+    for (i = 0; i < el.length; i++) {
+        array = array.filter(x => x !== el[i]);
+    }
+    //console.log(array)
+    return array;
 };
 
 // Do not edit below this line
