c82eba4cb93733b6423cf8a15c747036f5f1f57a
whobrien <whotemp@hotmail.com>
2022-03-11 10:20:07 -0500

    7 exercise Passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  const cel = (far - 32) * (5/9);
+  return Math.ceil(cel * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  const far = (cel * (9/5)) + 32;
+  return Math.ceil(far * 10) / 10;
 };
 
 // Do not edit below this line
