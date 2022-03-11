dec5a42f69331bbbbd4bc8798b6537e5dc33b347
whobrien <whotemp@hotmail.com>
2022-03-11 10:00:17 -0500

    6 exercise Passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(cYear) {
+    if (cYear % 4 == 0 && cYear % 100 !== 0) {
+        return true;
+    }
+    else if (cYear % 100 == 0 && cYear % 400 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
